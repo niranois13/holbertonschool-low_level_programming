e8dbe857f0c67237966d71e77917a0e5d1336e78
niranois13 <8784@holbertonstudents.com>
2024-02-19 21:19:36 +0100

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 /**
 * main - Entry point
-* Return: 0 if success
+* Return: 1 if success
 */
 int main(void)
 {
-	char msg[99];
-	strcpy(msg, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	char * write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
