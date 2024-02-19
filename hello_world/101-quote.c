b6722d3e66531e85d3ffaa75a0cde9859582b052
niranois13 <8784@holbertonstudents.com>
2024-02-19 17:00:01 +0100

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-	char("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0)
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }
