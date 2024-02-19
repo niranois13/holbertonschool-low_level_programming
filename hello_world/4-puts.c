c7b20c9edf0295e2d3d3ce4190a4e4da24037d60
niranois13 <8784@holbertonstudents.com>
2024-02-19 16:01:45 +0100

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 if success
+*/
 int main(void)
-/** Write a C program that prints a text */
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
