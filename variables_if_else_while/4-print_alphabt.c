27336e43ac881316224615218fb81e6687366c34
niranois13 <8784@holbertonstudents.com>
2024-02-20 19:10:54 +0100

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: This program will print the whole alphabet in lower case
+* Return: 0 if success
+*/
+
+int main(void)
+
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
