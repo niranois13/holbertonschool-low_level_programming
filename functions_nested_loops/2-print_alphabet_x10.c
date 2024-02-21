f8b0cb0b6306b52693a539520e7d5964bc0995ea
niranois13 <8784@holbertonstudents.com>
2024-02-21 20:36:22 +0100

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints the alphabet 10 times
+* Return: 0 if success
+*/
+
+void print_alphabet_x10(void);
+
+{
+	int l;
+	char letter;
+	for (l = 0 ; l == 10 ; l++)
+	{
+		for (letter = 'a' ; letter <= 'z' ; ++letter)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
