549456ecd9014852439cc3b07668da5ec1249977
niranois13 <8784@holbertonstudents.com>
2024-02-21 20:10:30 +0100

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet - Entry point
+* print_alphabet - main function
 * Description: Prints the alphabet
 * Return: 0 if success
 */
 void print_alphabet(void)
 
 {
-	char letter = 'a';
+	char letter;
 
 	for (letter = 'a' ; letter <= 'z' ; ++letter)
 	{
