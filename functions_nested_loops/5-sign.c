38a1f08ab91c55adce848e0f87358ecd72ef7452
niranois13 <8784@holbertonstudents.com>
2024-02-22 13:18:53 +0100

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - determines if 'n' is positive or negative +
+* and print '+' or '-' accordingly
+* @n : character to b determined
+* Return: 1 if 'n' is positive, -1 if 'n' is negative, 0 if 'n' is 0
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
