9a588021ef40513cb5cdfd4f5a56497e80fa5bbe
niranois13 <8784@holbertonstudents.com>
2024-02-22 17:49:51 +0100

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,19 +14,29 @@ void times_table(void)
 		b = 0;
 		do {
 			int produit = a * b;
-			if (produit < 10)
+
+			if (produit < 10 && b < 9)
 			{
 				_putchar('0' + produit);
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 			}
-			else
+			else if (produit >= 10 && b < 9)
 			{
 				_putchar(produit / 10 + '0');
 				_putchar(produit % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
+			}
+			else if (produit < 10 && b == 9)
+			{
+				_putchar('0' + produit);
+			}
+			else if (produit >= 10 && b == 9)
+			{
+				_putchar(produit / 10 + '0');
+				_putchar(produit % 10 + '0');
 			}
 		b++;
 		} while (b <= 9);
