4b59efccc94617599324d78c8fcbfbdab730f0a5
niranois13 <8784@holbertonstudents.com>
2024-02-23 10:45:14 +0100

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* print_to_98 - Prints all numbers from any int to 98.
+* @n : starting point
+*/
+void print_to_98(int n)
+
+{
+	for (; n <= 98; n++)
+	{
+		printf("%d, \n", n);
+	}
+	for (; n >= 98; n--)
+	{
+		printf("%d, \n", n);
+	}
+}
