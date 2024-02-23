ded2fd30969de448c107d2db57e4d4fbbdb21d68
niranois13 <8784@holbertonstudents.com>
2024-02-23 10:46:10 +0100

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,11 +7,11 @@
 void print_to_98(int n)
 
 {
-	for (; n <= 98; n++)
+	for (; n < 98; n++)
 	{
 		printf("%d, \n", n);
 	}
-	for (; n >= 98; n--)
+	for (; n > 98; n--)
 	{
 		printf("%d, \n", n);
 	}
