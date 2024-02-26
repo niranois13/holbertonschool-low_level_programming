e2c1984dd78692f7f34402b3761089063d787d44
niranois13 <8784@holbertonstudents.com>
2024-02-26 14:38:23 +0100

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - main function
+* Description: Prints the alphabet
+* Return: void
+*/
+void print_alphabet(void)
+
+{
+	int a;
+
+	for (a = '0' ; a <= '9' ; ++a)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+
+}
