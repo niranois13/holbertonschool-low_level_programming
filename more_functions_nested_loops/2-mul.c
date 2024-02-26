960f4c3354099611fdff6700565ca7bf5152fc16
niranois13 <8784@holbertonstudents.com>
2024-02-26 14:18:59 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _mult - multiplies two integer
+* @n : first integer
+* @m : second integer
+* @r : the result
+* Return: result of the multiplication
+*/
+
+int _mult(int n, int m, int r)
+
+{
+	int n, int m, int r;
+	{
+		res = (num1 * num2);
+	}
+	return (res);
+}
