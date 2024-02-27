744e73895c6e1a5f21e1a718451ef7b7c4d0077d
niranois13 <8784@holbertonstudents.com>
2024-02-27 12:44:42 +0100

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_square - prints squares based on the value of 'size'
+* @size : size of the suare to be drawn
+* Return: void
+*/
+void print_square(int size)
+
+{
+	int line = 0;
+	int row = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (row = 0 ; row <= size ; row++)
+	{
+		for (line = 0 ; line <= size ; line++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
