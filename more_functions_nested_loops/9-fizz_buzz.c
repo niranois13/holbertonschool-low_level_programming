898292807ad9073c596528f658dd5bacfa49ba85
niranois13 <8784@holbertonstudents.com>
2024-02-27 13:34:01 +0100

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: This will print a text,
+* replacing multiples of 3 by "Fizz",
+* multiples of 5 by "Buzz" and multiples of both by "FizzBuzz"
+* Return: 0 if success
+*/
+int main(void)
+
+{
+	int n;
+
+	for (n = 1 ; n <= 100 ; ++n)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
