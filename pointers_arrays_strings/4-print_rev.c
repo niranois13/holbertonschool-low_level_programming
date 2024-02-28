d33254008fe4576a0e39ae98b8d23ae4fcc2d6a3
niranois13 <8784@holbertonstudents.com>
2024-02-28 12:18:18 +0100

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_rev - prints a string in reverse
+*
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	while (l >= '\0')
+	{
+		_putchar(s[l]);
+		l--;
+	}
+	_putchar('\n');
+
+
+}
