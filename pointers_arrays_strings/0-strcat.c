1e7baca7a4fb1a9f3bc6f95b765e8f29cdf222f1
niranois13 <8784@holbertonstudents.com>
2024-03-01 13:11:47 +0100

    00

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,17 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 	{
+		++dest;
 		i++;
 	}
 
 	while (src[j] != '\0')
 	{
-		dest[i++] = src[j++];
+		*dest = *src;
+		dest++;
+		src++;
+		j++;
 	}
-
-	dest[i] = '\0';
 
 	return (dest);
 }
