aba2c0a7d6777b91e54e209f287815a241a85dc5
niranois13 <8784@holbertonstudents.com>
2024-03-01 13:30:39 +0100

    11

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strncat - main function that concatenates two string
+* @src : string to be added
+* @dest : strings elements are bing added to
+* @n : number of bytes from *src to be added
+*/
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i;
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	return (result);
+}
