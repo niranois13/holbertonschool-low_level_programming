2468a41de4d975906e6e7fe1ca7c3f9458171dc6
niranois13 <8784@holbertonstudents.com>
2024-03-01 15:09:11 +0100

    33

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,21 +3,20 @@
 * _strcmp - main function that compares two strings
 * @s1 : first string
 * @s2 : second string
-* Return: result
+* Return: < 0 if s1 is less than s2, 0 if s1 is equal to s2, > 0 if s1 is greater than s2
 */
 int _strcmp(char *s1, char *s2)
 
 {
 	int i = 0;
-	int result;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			result = (s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (result);
+	return (s1[i] - s2[i]);
 }
