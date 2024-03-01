b27b6b51eb042d88febed2e877b6e5afc59d0503
niranois13 <8784@holbertonstudents.com>
2024-03-01 15:35:18 +0100

    44

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* reverse_array - main function that prints values of an array in reverse
+* @*a : array to be reversed
+* @n : number of elements to print
+* Return: void
+*/
+void reverse_array(int *a, int n)
+
+{
+	int l;
+	int temp;
+
+	for (l = 0 ; l < (n / 2); l++)
+	{
+		temp = a[l];
+		a[l] = a[n - l - 1];
+		a[n - l - 1] = temp;
+	}
+}
