f00344faac921511eb0a79c558eadcfd878ec0e1
niranois13 <8784@holbertonstudents.com>
2024-03-01 15:10:06 +0100

    33

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,9 @@
 * _strcmp - main function that compares two strings
 * @s1 : first string
 * @s2 : second string
-* Return: < 0 if s1 is less than s2, 0 if s1 is equal to s2, > 0 if s1 is greater than s2
+* Return: 	< 0 if s1 is less than s2,
+*			0 if s1 is equal to s2,
+*			> 0 if s1 is greater than s2
 */
 int _strcmp(char *s1, char *s2)
 
