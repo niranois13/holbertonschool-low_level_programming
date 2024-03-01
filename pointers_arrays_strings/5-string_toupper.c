7a9a3efcc180696356a08e0242dd84616731dd06
niranois13 <8784@holbertonstudents.com>
2024-03-01 15:53:30 +0100

    55

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* string_toupper - function that converts all lowercase car to uppecase
+* @str : string to be converted
+* Return: char
+*/
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
