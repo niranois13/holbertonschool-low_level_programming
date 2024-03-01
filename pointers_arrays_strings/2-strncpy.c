a421265300d0c31411f4f21340e738ee5029200a
niranois13 <8784@holbertonstudents.com>
2024-03-01 14:15:54 +0100

    22

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
-*
-*
-*
-*
+* _strncpy - function that copies "n" bytes of a string to another
+* @dest : variable in which elements are copied to
+* @src : variable source of the element to copy
+* @n : number of source element to copy
+* Return: dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-   for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
-    for ( ; i < n; i++)
-        dest[i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-   return dest;
+	for ( ; i < n; i++)
+	{
+	dest[i] = '\0';
+	}
+
+	return (dest);
 }
