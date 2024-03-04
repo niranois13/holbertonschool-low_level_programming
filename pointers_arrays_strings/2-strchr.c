4a56d162cfa0d9f22af92ce3c55cfb79318eb893
niranois13 <8784@holbertonstudents.com>
2024-03-04 14:26:38 +0100

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 * _strchr - locates a character 'c' in a string 's'.
 * @s : string
 * @c : character to be found in string
-* Return: 	pointer ton the location of the first character
+* Return:	pointer ton the location of the first character
 *			NULL if character not found
 */
 char *_strchr(char *s, char c)
