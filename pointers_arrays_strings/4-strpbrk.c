ec9cbd5ce31ab8aa77d0ff0b585af9a5972f2e78
niranois13 <8784@holbertonstudents.com>
2024-03-05 10:38:38 +0100

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strpbrk - gets the length of a prefix substring.
+* @s: string
+* @accept: substring
+* Return: length of the matching substring
+*/
+char *_strpbrk(char *s, char *accept)
+
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
