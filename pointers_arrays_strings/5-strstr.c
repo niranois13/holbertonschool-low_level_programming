f565cc88dba8dd6a4e1a494d6d6093bab2c89a5c
niranois13 <8784@holbertonstudents.com>
2024-03-05 11:42:08 +0100

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-*
-*
-*
-*
+* _strstr - function that locates a substring
+* @needle: the substring to locate
+* @haystack: the string
+* Return:	pointer to the beginning of the substring
+*			NULL if no match or empty substring
 */
 char *_strstr(char *haystack, char *needle)
 
