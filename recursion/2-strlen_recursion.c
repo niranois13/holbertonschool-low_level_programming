ff2451f732f00dcf78c141d59295e72b289f1dfe
niranois13 <8784@holbertonstudents.com>
2024-03-08 13:52:54 +0100

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,15 +5,9 @@
 * Return: length of the string
 */
 int _strlen_recursion(char *s)
-
 {
-	int len = 0;
-
-	if (*s != '\0')
-	{
-		len ++;
-	}
-
-	_strlen_recursion(s++);
-	return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
