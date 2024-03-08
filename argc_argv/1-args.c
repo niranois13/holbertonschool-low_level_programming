83179f3a71fa6f29a4bd360b30a7faff48554c4e
niranois13 <8784@holbertonstudents.com>
2024-03-08 18:31:19 +0100

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,12 +7,8 @@
 * @argc: argument count
 * Return: 0 if success
 */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int i = 0;
-	if (argv[i] > 0)
-	{
-		printf("%d\n", argc);
-	}
+	printf("%d\n", argc);
 	return (0);
 }
