00737ea83a8269a4dce9d5b188c8a42fa6a79278
niranois13 <8784@holbertonstudents.com>
2024-03-19 14:55:11 +0100

    8765

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	a = atoi(argv[1]);
 	s = argv[2];
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -47,7 +47,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -64,7 +64,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (100);
+		exit(100);
 	}
 	return (a % b);
 }
