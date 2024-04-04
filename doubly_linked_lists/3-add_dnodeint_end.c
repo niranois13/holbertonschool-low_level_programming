e37bcc3f801b21739cca863229d51a875a6565f5
niranois13 <8784@holbertonstudents.com>
2024-04-04 19:59:14 +0200

    3-max

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,7 +6,7 @@
 * @n: an integer
 * Return: address of the newly added node
 */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+/** dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *tail;
@@ -31,9 +31,38 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	tail->next = new_node;
-
 	new_node->prev = tail;
 	new_node->next = NULL;
 
+	return (new_node);
+}
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		last_node = *head;
+
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
 	return (new_node);
 }
