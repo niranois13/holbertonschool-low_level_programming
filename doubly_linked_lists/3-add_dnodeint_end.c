83622752a9e18a9cdb7523dcf9fb5d7fc9c4419c
niranois13 <8784@holbertonstudents.com>
2024-04-04 23:49:51 +0200

    3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,47 +2,16 @@
 
 /**
 * add_dnodeint_end - adds a node at the end of a doubly linked list
-* @head: pointer to the first node of the list
-* @n: an integer
-* Return: address of the newly added node
+* @head: pointer to the pointer to the first node of the list
+* @n: the integer to store in the new node
+* Return: address of the newly added node or NULL on failure
 */
-/** dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *tail;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->prev = NULL;
-	}
-
-	tail = *head;
-
-	while (tail->next != NULL)
-	{
-		tail = tail->next;
-	}
-
-	tail->next = new_node;
-	new_node->prev = tail;
-	new_node->next = NULL;
-
-	return (new_node);
-}
-*/
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new_node, *last_node;
-
-	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 	{
 		free(new_node);
@@ -53,16 +22,20 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		last_node = *head;
-
-		while (last_node->next != NULL)
-			last_node = last_node->next;
-
-		last_node->next = new_node;
-		new_node->prev = last_node;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
 	}
+
+	tail = *head;
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+
+	tail->next = new_node;
+	new_node->prev = tail;
+
 	return (new_node);
 }
