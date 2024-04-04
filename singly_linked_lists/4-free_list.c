0f91a306c1c8f96cb603b40022a4e0ee65267997
niranois13 <8784@holbertonstudents.com>
2024-04-04 14:23:43 +0200

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+* free_list - function that frees allocated memory of a linked list
+* @head: pointer to the first node of the linked list
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head->next;
+	free(head->str);
+	free(head);
+	head = tmp;
+}
