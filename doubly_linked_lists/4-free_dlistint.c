547a48696c072e258d3c6172ab2f700be4597f0e
niranois13 <8784@holbertonstudents.com>
2024-04-05 00:04:28 +0200

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_dlistint - function that frees allocated memory of a double linked list
+* @head: pointer to the first node of this list
+*
+* Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->prev);
+		free(head);
+		head = tmp;
+	}
+}
