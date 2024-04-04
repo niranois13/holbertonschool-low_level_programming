aa8a6b211986dd8b943f33b98db531f73ecc1df6
niranois13 <8784@holbertonstudents.com>
2024-04-04 14:27:44 +0200

    4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,8 +8,12 @@ void free_list(list_t *head)
 {
 	list_t *tmp;
 
-	tmp = head->next;
-	free(head->str);
-	free(head);
-	head = tmp;
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
+
