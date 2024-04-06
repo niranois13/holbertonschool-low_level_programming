cdf8176025b8ea0ec03d6e080bc183552605125e
niranois13 <8784@holbertonstudents.com>
2024-04-06 23:12:40 +0200

    7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,38 +9,31 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
+	dlistint_t *current = *h, *new;
 
-	if (h == NULL)
-		return (NULL);
-
-	current = *h;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (idx == 0 || *h == NULL)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx != 0 && current->next != NULL)
+	while (idx != 1)
 	{
 		current = current->next;
+		if (current == NULL)
+			return (NULL);
 		idx--;
 	}
 
-	if (idx > 0)
-	{
-		free(new_node);
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	}
 
-		new_node->next = current->next;
-		new_node->prev = current;
-		current->next->prev = new_node;
-		current->next = new_node;
+	new->n = n;
+	new->prev = current;
+	new->next = current->next;
+	current->next->prev = new;
+	current->next = new;
 
-	return (new_node);
+	return (new);
 }
